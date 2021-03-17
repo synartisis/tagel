aaafb7e48e126bc1f02715becb2a27470badadca
mike <mgalakos@gmail.com>
2021-03-17 13:12:51 +0200

    rename webo to tg

--- tagel-parser.js
+++ tagel-parser.js
@@ -3,19 +3,19 @@ import { readFile } from 'fs/promises'
 import * as parse5 from './parse5.js'
 
 
-export async function applyTagel(doc, filename, partials, weboContext) {
-  await weboInclude(doc, filename, partials)
-  await weboEnv(doc, process.env.NODE_ENV === 'production' ? 'production' : 'development')
-  await weboBind(doc, weboContext)
-  await weboIf(doc, weboContext)
-  await weboFor(doc, weboContext)
-  await weboLang(doc, weboContext)
+export async function applyTagel(doc, filename, partials, tgContext) {
+  await tgImport(doc, filename, partials)
+  await tgEnv(doc, process.env.NODE_ENV === 'production' ? 'production' : 'development')
+  await tgBind(doc, tgContext)
+  await tgIf(doc, tgContext)
+  await tgFor(doc, tgContext)
+  await tgLang(doc, tgContext)
 }
 
 
 
-async function weboInclude(doc, filename, partials) {
-  const refs = parse5.qsa(doc, el => el.name === 'link' && el.attribs['rel'] === 'webo-include')
+async function tgImport(doc, filename, partials) {
+  const refs = parse5.qsa(doc, el => el.name === 'link' && el.attribs['rel'] === 'import')
   await Promise.all(
     refs.map(async el => {
       const { href } = el.attribs
@@ -31,28 +31,28 @@ async function weboInclude(doc, filename, partials) {
   )
 }
 
-async function weboEnv(doc, currentEnv) {
-  const els = parse5.qsa(doc, el => el.attribs && 'webo-env' in el.attribs)
+async function tgEnv(doc, currentEnv) {
+  const els = parse5.qsa(doc, el => el.attribs && 'tg-env' in el.attribs)
   await Promise.all(
     els.map(el => {
-      if (el.attribs['webo-env'] !== currentEnv) {
+      if (el.attribs['tg-env'] !== currentEnv) {
         parse5.remove(el)
       } else {
-        delete el.attribs['webo-env']
+        delete el.attribs['tg-env']
       }
     })
   )
 }
 
-async function weboBind(doc, weboContext) {
-  // console.log({weboContext})
-  if (!weboContext) return
-  const els = parse5.qsa(doc, el => el.attribs && 'webo-bind' in el.attribs)
+async function tgBind(doc, tgContext) {
+  // console.log({tgContext})
+  if (!tgContext) return
+  const els = parse5.qsa(doc, el => el.attribs && 'tg-bind' in el.attribs)
   await Promise.all(
     els.map(el => {
-      const varName = el.attribs['webo-bind']
-      const value = weboContext[varName]
-      // delete el.attribs['webo-bind']
+      const varName = el.attribs['tg-bind']
+      const value = tgContext[varName]
+      // delete el.attribs['tg-bind']
       if (value != null) {
         const content = parse5.createTextNode(value)
         parse5.append(el, content)
@@ -61,34 +61,34 @@ async function weboBind(doc, weboContext) {
   )
 }
 
-async function weboIf(doc, weboContext) {
-  if (!weboContext) return
-  const els = parse5.qsa(doc, el => el.attribs && 'webo-if' in el.attribs)
+async function tgIf(doc, tgContext) {
+  if (!tgContext) return
+  const els = parse5.qsa(doc, el => el.attribs && 'tg-if' in el.attribs)
   await Promise.all(
     els.map(el => {
-      const property = el.attribs['webo-if']
-      if (property in weboContext) {
-        const value = weboContext[property]
+      const property = el.attribs['tg-if']
+      if (property in tgContext) {
+        const value = tgContext[property]
         if (!value) el.attribs['hidden'] = ''
-        // delete el.attribs['webo-if']
+        // delete el.attribs['tg-if']
         // if (!value) parse5.remove(el)
       } else {
-        console.error(`[webo-if] value for property "${property}" not found`)
+        console.error(`[tg-if] value for property "${property}" not found`)
       }
     })
   )
 }
 
-async function weboFor(doc, weboContext) {
-  if (!weboContext) return
-  const els = parse5.qsa(doc, el => el.attribs && 'webo-for' in el.attribs)
+async function tgFor(doc, tgContext) {
+  if (!tgContext) return
+  const els = parse5.qsa(doc, el => el.attribs && 'tg-for' in el.attribs)
   await Promise.all(
     els.map(async el => {
-      const property = el.attribs['webo-for']
-      if (property in weboContext) {
-        const value = weboContext[property]
+      const property = el.attribs['tg-for']
+      if (property in tgContext) {
+        const value = tgContext[property]
         if (!Array.isArray(value)) {
-          console.error(`[webo-for] value for property "${property}" is not array`)
+          console.error(`[tg-for] value for property "${property}" is not array`)
           return
         }
         let lastEl = el
@@ -96,23 +96,23 @@ async function weboFor(doc, weboContext) {
           // console.log({$item, $index})
           const itemTemplate = parse5.clone(el)
           itemTemplate.attribs['check'] = String($index)
-          await weboBind(itemTemplate, $item)
+          await tgBind(itemTemplate, $item)
           parse5.insertAfter(itemTemplate, lastEl)
           lastEl = itemTemplate
         }
         parse5.remove(el)
       } else {
-        console.error(`[webo-for] value for property "${property}" not found`)
+        console.error(`[tg-for] value for property "${property}" not found`)
       }
     })
   )
 }
 
-async function weboLang(doc, weboContext) {
+async function tgLang(doc, tgContext) {
   const htmlTag = parse5.qs(doc, el => el.name === 'html')
   if (!htmlTag) return
-  if (weboContext.lang) {
-    htmlTag.attribs['lang'] = weboContext.lang
+  if (tgContext.lang) {
+    htmlTag.attribs['lang'] = tgContext.lang
   }
   const lang = htmlTag.attribs['lang']
   const els = parse5.qsa(doc, el => el.attribs && 'lang' in el.attribs && el.attribs['lang'] !== lang)
@@ -136,11 +136,10 @@ async function rewritePartials(doc, relPath) {
       const attr = 'src' in el.attribs ? 'src' : 'href'
       const uri = el.attribs[attr]
       const ref = uri && uri.split('?')[0]
-      if (!ref) console.log(el)
-      console.log(el.name, {uri,ref})
+      // console.log(el.name, {uri,ref})
       if (ref.startsWith('http://') || ref.startsWith('https://') || ref.startsWith('/')) return
       const refNew = path.join(relPath, ref)
-      console.log('--', refNew, relPath)
+      // console.log('--', refNew, relPath)
       el.attribs[attr] = refNew
     })
   )    
--- tagel.js
+++ tagel.js
@@ -9,7 +9,7 @@ export function tagelExpress(root, {} = {}) {
     const { filename, content } = await readFileContent(root, req.url)
     if (content !== undefined) {
       const doc = parse5.parseHtml(content)
-      await applyTagel(doc, filename, {}, req.weboContext)
+      await applyTagel(doc, filename, {}, req.tgContext)
       return res.send(parse5.serialize(doc))
     }
     return next()
@@ -21,12 +21,16 @@ export function tagelExpress(root, {} = {}) {
 async function readFileContent(root, url) {
   const fileUrl = url.split('?')[0]
   const filename = path.join(root, fileUrl)
-  const fileext = filename.split('/').pop().split('.').pop()
+  const fileext = path.extname(filename.split('/').pop())
   if (fileext === '') {
-    const filestat = await stat(filename)
-    if (filestat.isDirectory()) return readFileContent(root, path.join(url, 'index.html'))
+    let filestat
+    try {
+      filestat = await stat(filename)
+    } catch (error) {}
+    if (filestat?.isDirectory()) return readFileContent(root, path.join(url, 'index.html'))
+    return readFileContent(root, url + '.html')
   }
-  if (fileext === 'html') {
+  if (fileext === '.html') {
     return { filename, content: await readFile(filename, 'utf-8') }
   }
   return {}
